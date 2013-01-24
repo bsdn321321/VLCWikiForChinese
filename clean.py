bfb72a83add817c267c8c7f69a98dc9891356ac4
pangliang <pangliang@pangliang-asus.(none)>
2013-01-24 10:09:03 +0800

    用于清除已生成的.html文件:   clean.py 	将.md文件批量编译成.html文件，生成的文件与对应的.md文件同目录:   compile.py 	适合于喜欢configure->make的同志，把编译脚本放到makefile里:   configure.py

---
+++ clean.py
@@ -0,0 +1,2 @@
+import os
+os.system('find -name \'*.html\' -exec rm {} \\;')
---
+++ configure.py
@@ -0,0 +1,18 @@
+import os
+filelist=os.popen('find -name \'*.md\'').readlines()
+cmd1=''
+cmd2=''
+cmd3=''
+#os.system('echo "all:\\n" > makefile')
+file_make=open('makefile','w')
+file_make.write('all:')
+for filename in filelist:
+    filename=filename[2:-4]
+    cmd1='echo "<html><head><meta http-equiv=\\"Content-Type\\" content=\\"text/html; charset=UTF-8\\"></head><body>\\n" > '+filename+'.html'
+    cmd2='markdown '+filename+'.md >>'+filename+'.html'
+    cmd3='echo "\\n</body></html>" >> '+filename+'.html'
+    #print cmd1
+    #print cmd2
+    #print cmd3
+    file_make.write('\n\t'+cmd1+';'+cmd2+';'+cmd3)
+#file_make.write('\n')
